61275ad3c45efc30fed2b53d565fc12f3e779921
Ayodele Smith-Jackson <ayodele@trellahealth.com>
2024-01-07 02:32:05 -0500

    minor unit test cleanup

--- backend/src/journal/journal.controller.spec.ts
+++ backend/src/journal/journal.controller.spec.ts
@@ -5,7 +5,6 @@ import { GetOneItem } from '../universal/getSingle.model';
 import { JournalController } from './journal.controller';
 import { JournalService } from './journal.service';
 import { JournalEntry } from './journalEntry.model';
-import { JournalTradeId } from './journalTradeId.model';
 
 jest.mock('./journal.service');
 jest.mock('../guards/auth.guard');
@@ -58,29 +57,6 @@ describe('JournalController', () => {
     });
   });
 
-  describe('Journal Controller - Delete Journal Entry', () => {
-    it('should delete an entry', async () => {
-      const result = true;
-      jest.spyOn(service, 'delete').mockResolvedValue(result);
-
-      expect(await controller.delete(req, mockItem)).toBe(result);
-      expect(service.delete).toHaveBeenCalledTimes(1);
-    });
-
-    it('should fail to delete an entry', async () => {
-      jest
-        .spyOn(service, 'delete')
-        .mockResolvedValue(
-          Promise.reject(new InternalServerErrorException('')),
-        );
-
-      await expect(controller.delete(req, mockItem)).rejects.toThrow(
-        InternalServerErrorException,
-      );
-      expect(service.delete).toHaveBeenCalledTimes(1);
-    });
-  });
-
   describe('Journal Controller - Edit Journal Entry', () => {
     it('should edit an entry', async () => {
       const result = true;
@@ -126,54 +102,6 @@ describe('JournalController', () => {
         InternalServerErrorException,
       );
       expect(service.getAll).toHaveBeenCalledTimes(1);
-    });
-  });
-
-  describe('Journal Controller - Link Journal Entry', () => {
-    it('should link a trade/option', async () => {
-      const result = true;
-      jest.spyOn(service, 'linkTrade').mockResolvedValue(result);
-
-      expect(
-        await controller.linkTrade(req, mockItem, new JournalTradeId()),
-      ).toBe(result);
-      expect(service.linkTrade).toHaveBeenCalledTimes(1);
-    });
-
-    it('should fail to link a trade/option', async () => {
-      jest
-        .spyOn(service, 'linkTrade')
-        .mockResolvedValue(
-          Promise.reject(new InternalServerErrorException('')),
-        );
-
-      await expect(
-        controller.linkTrade(req, mockItem, new JournalTradeId()),
-      ).rejects.toThrow(InternalServerErrorException);
-      expect(service.linkTrade).toHaveBeenCalledTimes(1);
-    });
-  });
-
-  describe('Journal Controller - Unlink Journal Entry', () => {
-    it('should unlink a trade', async () => {
-      const result = true;
-      jest.spyOn(service, 'unlinkTrade').mockResolvedValue(result);
-
-      expect(await controller.unlinkTrade(req, mockItem)).toBe(result);
-      expect(service.unlinkTrade).toHaveBeenCalledTimes(1);
-    });
-
-    it('should fail to unlink a trade', async () => {
-      jest
-        .spyOn(service, 'unlinkTrade')
-        .mockResolvedValue(
-          Promise.reject(new InternalServerErrorException('')),
-        );
-
-      await expect(controller.unlinkTrade(req, mockItem)).rejects.toThrow(
-        InternalServerErrorException,
-      );
-      expect(service.unlinkTrade).toHaveBeenCalledTimes(1);
     });
   });
 });
